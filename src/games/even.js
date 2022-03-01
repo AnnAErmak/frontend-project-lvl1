220416fa3db90ff160aed9c9be44d7aa5a34dc59
Anna Ermak <ermak-anna86@yandex.ru>
2022-03-01 19:47:54 +0300

    refactoring even

--- src/games/even.js
+++ src/games/even.js
@@ -1,92 +1,17 @@
-import {helloGamer, startRounds} from "../index.js";
-import readlineSync from "readline-sync";
+import startRounds from '../index.js';
+import { randomValue } from '../utils/utils.js';
 
-
-export function startGameEven(){
-    // let counter = 0
-    const gamerName = helloGamer()
-    console.log('Answer "yes" if the number is even, otherwise answer "no".')
-    startRounds(askQuestion, gamerName)
-    // for (let i = 0; i < 3; i++){
-    //     if(askQuestion() === 'stop'){
-    //         stopGame(gamerName)
-    //         break
-    //     }
-    //     counter ++
-    // }
-    // if (counter === 3) console.log(`Congratulations, ${gamerName}!`)
+function isEven(number) {
+  return !(number % 2);
 }
-
-function askQuestion(){
-    const randNumber = Math.floor(Math.random() * 10)
-    console.log(`Question: ${randNumber}`);
-    const answer = readlineSync.question('You answer: ');
-    if (answer === 'yes' || answer === 'no') {
-
-        if (randNumber % 2 === 0 && answer === 'yes') console.log('Correct!')
-        if (randNumber % 2 !== 0 && answer === 'no')  console.log('Correct!')
-
-        if (randNumber % 2 === 0 && answer === 'no') {
-            console.log('\'no\' is wrong answer ;(. Correct answer was \'yes\'.')
-            return 'stop'
-        }
-        if (randNumber % 2 !== 0 && answer === 'yes') {
-            console.log('\'yes\' is wrong answer ;(. Correct answer was \'no\'.')
-            return 'stop'
-        }
-    }else {
-        return 'stop'
-    }
+function askQuestion() {
+  const randNumber = randomValue(10);
+  const question = `Question: ${randNumber}`;
+  const correctAnswer = (isEven(randNumber)) ? 'yes' : 'no';
+  return [question, correctAnswer];
 }
+export default function startGameEven() {
+  const task = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-
-
-/*
-(function startGame(){
-    let counter = 0
-    console.log('Welcome to the Brain Games!')
-    let gamerName = getGamerName() || 'аноним'
-    console.log(`Hello, ${gamerName}!`)
-    console.log('Answer "yes" if the number is even, otherwise answer "no".')
-    for (let i = 0; i < 3; i++){
-        if(askQuestion() === 'stop'){
-            stopGame(gamerName)
-            break
-        }
-        counter ++
-    }
-
-    if (counter === 3) console.log(`Congratulations, ${gamerName}!`)
-})()
-
-
-function getGamerName(){
-    return readlineSync.question('May I have your name? ');
+  startRounds(task, askQuestion);
 }
-
-function askQuestion(){
-    const randNumber = Math.floor(Math.random() * 10)
-    console.log(`Question: ${randNumber}`);
-    const answer = readlineSync.question('You answer: ');
-    if (answer === 'yes' || answer === 'no') {
-
-        if (randNumber % 2 === 0 && answer === 'yes') console.log('Correct!')
-        if (randNumber % 2 !== 0 && answer === 'no')  console.log('Correct!')
-
-        if (randNumber % 2 === 0 && answer === 'no') {
-            console.log('\'no\' is wrong answer ;(. Correct answer was \'yes\'.')
-            return 'stop'
-        }
-        if (randNumber % 2 !== 0 && answer === 'yes') {
-            console.log('\'yes\' is wrong answer ;(. Correct answer was \'no\'.')
-            return 'stop'
-        }
-    }else {
-        return 'stop'
-    }
-}
-
-function stopGame(name){
-    console.log(`Let\'s try again, ${name}`)
-}
- */
--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ export default function startRounds(task, getDataGame) {
   const gamerName = readlineSync.question('May I have your name? ') || 'аноним';
   console.log(`Hello, ${gamerName}!`);
   console.log(task);
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 3; i += 1) {
     const [question, correctAnswer] = getDataGame();
     console.log(question);
     const answer = readlineSync.question('You answer: ');
     if (answer === correctAnswer) {
       console.log('Correct!');
     } else {
-      console.log(`\'${answer}\' is wrong answer ;(. Correct answer was \'${correctAnswer}\'.`);
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${gamerName}!`);
       return;
     }
