42e05f9a8c49e7c3269e3c419cb993065431020d
Anna Ermak <ermak-anna86@yandex.ru>
2022-03-02 08:21:59 +0300

    refactoring brain-games, cli

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import getNameGamer from "../src/cli.js";
+import getNameGamer from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!')
-getNameGamer()
+console.log('Welcome to the Brain Games!');
+getNameGamer();
--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,10 @@
 import readlineSync from 'readline-sync';
 
-export default function getNameGamer(){
-    const name = readlineSync.question('May I have your name? ');
-    if (!name){
-        getNameGamer()
-    }else {
-        console.log(`Hello, ${name}!`)
-    }
+export default function getNameGamer() {
+  const name = readlineSync.question('May I have your name? ');
+  if (!name) {
+    getNameGamer();
+  } else {
+    console.log(`Hello, ${name}!`);
+  }
 }
-
