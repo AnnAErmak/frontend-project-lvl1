66d05098b86d844309ea05d6b5f16314e16bb9b8
Anna Ermak <ermak-anna86@yandex.ru>
2022-03-05 09:07:09 +0300

    refactoring index.js if-else, add const numberRounds

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import readlineSync from 'readline-sync';
 
+const numberRounds = 3;
+
 export default function startRounds(task, getDataGame) {
   console.log('Welcome to the Brain Games!');
   const gamerName = readlineSync.question('May I have your name? ') || 'аноним';
   console.log(`Hello, ${gamerName}!`);
   console.log(task);
-  for (let i = 0; i < 3; i += 1) {
+  for (let i = 0; i < numberRounds; i += 1) {
     const [question, correctAnswer] = getDataGame();
     console.log(question);
     const answer = readlineSync.question('You answer: ');
-    if (answer === correctAnswer) {
-      console.log('Correct!');
-    } else {
+    if (answer !== correctAnswer) {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${gamerName}!`);
       return;
     }
+    console.log('Correct!');
   }
   console.log(`Congratulations, ${gamerName}!`);
 }
